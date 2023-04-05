2256ce3617f7f8d8104a46289d42be63180f10bc
souravbiswassanto <saurov@appscode.com>
2023-04-05 17:40:46 +0600

    C3: Maps updated

--- _self3-maps/main.go
+++ _self3-maps/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	Map := make(map[string]int)
@@ -10,5 +12,23 @@ func main() {
 	for key, value := range Map {
 		fmt.Println(key, value)
 	}
+	val, ok := Map["sato"]
+	fmt.Println(val, ok)
+	if ok == true {
+		fmt.Println("okay")
+	}
 
+	// creating more complex maps
+
+	newmap := make(map[string]map[string]int)
+	innermap := make(map[string]int)
+	innermap["santo"] = 1
+	newmap["sourav"] = innermap
+
+	fmt.Println(newmap)
+	for key1, value := range newmap {
+		for key2, val := range value {
+			fmt.Println(key1, key2, val)
+		}
+	}
 }
